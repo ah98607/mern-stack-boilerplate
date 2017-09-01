e986ecf50c3c348751338824001d1c384b5c62e7
ah98607 <adh6107@gmail.com>
2017-09-01 02:00:57 -0700

    mongoose, express, react all worked

---
+++ routes/routes.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models/models.js')
+
+/* GET users listing. */
+router.get('/123', function(req, res, next) {
+	console.log("/123 called");
+	models.removeAll(function(data) {
+		models.add("Andy", function(data) {
+			models.all(function(data) {
+				console.log(data);
+				res.json(data);
+			});
+		});
+	});
+
+	/*res.json([{
+		id: 1,
+		username: "samsepi0l"
+	}, {
+		id: 2,
+		username: "D0loresH4ze"
+	}]);*/
+});
+
+module.exports = router;
